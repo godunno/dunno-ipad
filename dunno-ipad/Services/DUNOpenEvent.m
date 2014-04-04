72ed5085b35ddd93feda044f17f97c3ea9a06d7c
Fagner Moura <fagnermoura@gmail.com>
2014-04-04 18:37:11 -0300

    subscribe pusher events on timeline

--- dunno-ipad/Services/DUNOpenEvent.m
+++ dunno-ipad/Services/DUNOpenEvent.m
@@ -1,9 +1,14 @@
 #import "DUNOpenEvent.h"
+
 #import "DUNAPI.h"
+#import "DUNPusher.h"
 #import "DUNSession.h"
+
+#import "DUNTimelineUserMessage.h"
 
 @interface DUNOpenEvent()
 @property (nonatomic, strong) DUNEvent *event;
+@property (nonatomic, strong) DUNSession *session;
 @end
 
 @implementation DUNOpenEvent
@@ -14,6 +19,7 @@
   
   if ((self = [self init])){
     _event = event;
+    _session = [DUNSession sharedInstance];
   }
   
   return self;
@@ -24,11 +30,10 @@
   NSParameterAssert(_event!=nil);
   
   [[DUNAPI sharedInstance] openEvent:_event success:^(DUNEvent *eventOpened) {
-
-    DUNSession *session = [DUNSession sharedInstance];
-    session.activeEvent = eventOpened;
     
-    //sign all pusher operations.
+    _session.activeEvent = eventOpened;
+    
+    [self registerPusherEvents];
     
     //invalidate
     _event = nil;
@@ -40,5 +45,53 @@
   }];
   
 }
+
+# pragma mark -
+# pragma mark - Pusher events
+
+- (void) registerPusherEvents
+{
+  
+  NSParameterAssert(_event!=nil);
+  
+#if DEBUG
+  NSParameterAssert(_event.studentMessageEvent!=nil);
+  NSParameterAssert(_event.upDownVoteMessageEvent!=nil);
+#else
+  //TODO show error on modal dialog?
+#endif
+  
+  DUNPusher *pusher = [DUNPusher sharedInstance].connect;
+  
+  [pusher subscribeToChannelNamed:_event.channelName withEventNamed:_event.studentMessageEvent handleWithBlock:^(NSDictionary *jsonDictionary) {
+    
+    NSError *error = nil;
+    DUNTimelineUserMessage *newMessage = [MTLJSONAdapter modelOfClass:DUNTimelineUserMessage.class fromJSONDictionary:jsonDictionary error:&error];
+    
+#if DEBUG
+    NSAssert(error==nil, @"Error parsing JSON to DUNTimelineUserMessage on send Message to Timeline Pusher response");
+#else
+    //TODO show error on modal dialog?
+#endif
+    
+    [_session.activeEvent.timeline.messages addObject:newMessage];
+  }];
+  
+  [pusher subscribeToChannelNamed:_event.channelName withEventNamed:_event.upDownVoteMessageEvent handleWithBlock:^(NSDictionary *jsonDictionary) {
+    
+    NSError *error = nil;
+    DUNTimelineUserMessage *messageVoted = [MTLJSONAdapter modelOfClass:DUNTimelineUserMessage.class fromJSONDictionary:jsonDictionary error:&error];
+    
+#if DEBUG
+    NSAssert(error==nil, @"Error parsing JSON to DUNTimelineUserMessage on up/down vote Pusher response");
+#else
+    //TODO show error on modal dialog?
+#endif
+    
+    [_session.activeEvent.timeline updateMessage:messageVoted];
+  }];
+  
+}
+
 
 @end
